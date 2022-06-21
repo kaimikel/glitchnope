9a70953943a94f1d781dd9e10de05b3ff6dcfa4e
Glitch (glitch-blank-website) <none>
2022-06-21 19:29:25 +0000

    🎭💋 Checkpoint ./style.css:44930262/5125 ./rigcity.html:44930262/4876 ./script.js:44930262/2717

--- script.js
+++ script.js
@@ -2,3 +2,39 @@
     to your site with Javascript */
 
 console.log("Hello, world!");
+
+var galleryThumbs = new Swiper(".gallery-thumbs", {
+  centeredSlides: true,
+  centeredSlidesBounds: true,
+  slidesPerView: 3,
+  watchOverflow: true,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+  direction: "vertical",
+});
+
+var galleryMain = new Swiper(".gallery-main", {
+  watchOverflow: true,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+  preventInteractionOnTransition: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true,
+  },
+  thumbs: {
+    swiper: galleryThumbs,
+  },
+});
+
+galleryMain.on("slideChangeTransitionStart", function () {
+  galleryThumbs.slideTo(galleryMain.activeIndex);
+});
+
+galleryThumbs.on("transitionStart", function () {
+  galleryMain.slideTo(galleryThumbs.activeIndex);
+});
